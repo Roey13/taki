a7cff969c67032ca6ddddb83bb58440e3c0db785
Roey13 <brdroey@gmail.com>
2022-02-17 17:17:32 +0200

    legal moves 2.0

--- frontend/src/helpers/checkIfLegal.js
+++ frontend/src/helpers/checkIfLegal.js
@@ -3,24 +3,32 @@ export function checkIfLegal(card, playingDeck) {
     const currPLayingCard = playingDeck[0]
 
     const playingCardColors = currPLayingCard.cardColor
+    const playingCardShape = currPLayingCard.shape
+
     const playersCardColors = card.cardColor
+    const playersCardShape = card.shape
 
     let isLegal = false
 
-    if (playingCardColors.length > playersCardColors.length) {
-        playingCardColors.forEach((color) => {
-            if (playersCardColors.includes(color)) {
-                isLegal = true
-                return
-            }
-        })
+    if (playingCardShape === playersCardShape){
+        isLegal = true
+        return isLegal
     } else {
-        playersCardColors.forEach((color) => {
-            if (playingCardColors.includes(color)) {
-                isLegal = true
-                return
-            }
-        })
+        if (playingCardColors.length > playersCardColors.length) {
+            playingCardColors.forEach((color) => {
+                if (playersCardColors.includes(color)) {
+                    isLegal = true
+                    return
+                }
+            })
+        } else {
+            playersCardColors.forEach((color) => {
+                if (playingCardColors.includes(color)) {
+                    isLegal = true
+                    return
+                }
+            })
+        }
     }
 
     return isLegal
